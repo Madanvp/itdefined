05408937ff4865fe5473f1b618fe72a82f6928f1
Your Nameitd <your@example.com>
2024-01-19 21:44:04 +0530

    this new'

---
+++ data.js
@@ -0,0 +1,48 @@
+// // this is tutorial is about number and string
+// let height = 5.7
+// const weight = 70
+// console.log(typeof(height))
+// console.log(typeof(weight))
+
+// weight = 55
+// console.log(weight)
+
+
+// let name = "virat Kohle"
+// console.log(name)
+// console.log(typeof(name))
+
+
+//  let information = "hi my name is madan vp 'i am a boy'"
+//  console.log(information)
+//  console.log(typeof(information))
+
+// let information1 = "hi my name is madan vp \"i am a boy\""
+//  console.log(information1)
+//  console.log(typeof(information1))
+
+ 
+ // let information ="iam"
+ // console.log(information.length)
+
+ // let information ="iam"
+ // console.log(information[2])
+
+ // let information ="INDIA"
+ // console.log('The character present at 0th position is', information[0])
+
+  // console.log("Hello", "world", "1212234")
+
+  let data1 = "My name is madan vp"
+  console.log(data1)
+
+  let data2 = "My name is madan vp"
+  console.log(data2.length)
+
+  let data3 = 11
+  console.log(typeof(data3))
+
+  let data4 = "My mother name poornima bm"
+  console.log(data4[12])
+
+
---
+++ string.js
@@ -0,0 +1,83 @@
+// let country = 'India'
+// let data = `${country} launched attacks on tuestay`
+// console.log(data)
+
+// let a = 10
+// let b = 20
+// let sum = a + b
+// console.log(`The sum of ${a} and ${b} is ${sum}`)
+
+// string methods and string propertyes
+
+// let country = 'India'
+// console.log(`Length of the string country is ${country.length}
+
+
+// let country = 'India'
+// console.log(`Length of the string country is ${country.length}`)
+// let ucountry = country.toUpperCase()
+// console.log(ucountry)
+
+
+// let country = 'India'
+// console.log(`Length of the  country is ${country.length}`)
+
+// console.log(`The upper case version of ${country} is ${country.toUpperCase()}`)
+
+
+
+// let country = 'INDIA'
+// console.log(`Length of the  country is ${country.length}`)
+
+// console.log(`The lower case version of ${country} is ${country.toLowerCase()}`)
+
+
+
+// let country = 'INDIA'
+// console.log(`Length of the  country is ${country.length} and lower case of country ${country.toLowerCase()}`)
+
+
+// let country = 'INDIA'
+// console.log(country.length)
+
+
+// let country = 'india'
+// console.log(country.toLowerCase())
+
+
+// let country = 'INDIA'
+// console.log(country.toUpperCase())
+
+
+// let country = 'INDIA'
+// let ucountry = country[0].toLowerCase()
+// console.log(ucountry)
+
+
+  //  let data = 'Confusion followed the annoucement from Iran as state media report on it soon'
+  //  console.log(data.length)
+  // let output = data.substr(15,25)
+  //  console.log(output)
+
+  // let data = 'Confusion followed the annoucement from Iran as state media report on it soon'
+  // console.log(data.length)
+  // let output1 = data.substring(9,10)
+  // console.log(output1)
+  // let output2 = data.substring(10,22)
+  // console.log(output2)
+
+ // let data = 'Confusion followed the annoucement from Iran as state media report on it soon'
+ // console.log(data.length)
+ // let output = data.substr(6,12)
+ // console.log(output)
+
+
+let data = 'Confusion followed the annoucement from Iran as state media report on it soon'
+console.log(data.length)
+let output1 = data.substring(20,10)
+console.log(output1)
+
+  // let data = 'Confusion followed the annoucement from Iran as state media report on it soon'
+  // console.log(data.length)
+  // let output1 = data.substr(25,10)
+  // console.log(output1)
